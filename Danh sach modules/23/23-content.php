945c3ed44a40901515c357e1434f7d8c642d2abd
tuan <tuannt621>
2020-11-28 12:03:08 +0700

    commit module 23

---
+++ Danh sach modules/23/23-content.php
@@ -0,0 +1,17 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-23">
+    <input class="type-23__input" type="text" placeholder="Search">
+    <button class="type-23__search btn" type="button"></button>
+</div>
