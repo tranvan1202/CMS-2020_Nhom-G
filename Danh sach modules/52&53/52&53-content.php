4e27152c4a5a41b0e3550ab87c7c3335015c7417
Trần Văn <tranvan1202@users.noreply.github.com>
2020-12-14 17:08:10 +0700

    module 52&53

---
+++ Danh sach modules/52&53/52&53-content.php
@@ -0,0 +1,93 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-53">
+    <div class="container">
+        <div class="specialoffer-title">
+            <h4>SPECIAL OFFERS</h4>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="type-52">
+
+                    <div id="pgc-107-0-0" class="panel-grid-cell">
+                        <div id="panel-107-0-0-0" class="so-panel widget widget_sow-editor panel-first-child panel-last-child" data-index="0">
+                            <div class="so-widget-sow-editor so-widget-sow-editor-base">
+                                <div class="siteorigin-widget-tinymce textwidget">
+                                    <h5 style="text-align: left">
+                                        <a href="#" data-featherlight="image">
+                                            <img loading="lazy" class="alignnone wp-image-66 size-full" src="https://demo.proteusthemes.com/shaka/wp-content/uploads/sites/38/2016/06/1.jpg" alt="1" width="864" height="486" srcset="https://demo.proteusthemes.com/shaka/wp-content/uploads/sites/38/2016/06/1.jpg 864w, https://demo.proteusthemes.com/shaka/wp-content/uploads/sites/38/2016/06/1-300x169.jpg 300w, https://demo.proteusthemes.com/shaka/wp-content/uploads/sites/38/2016/06/1-768x432.jpg 768w, https://demo.proteusthemes.com/shaka/wp-content/uploads/sites/38/2016/06/1-540x304.jpg 540w, https://demo.proteusthemes.com/shaka/wp-content/uploads/sites/38/2016/06/1-352x198.jpg 352w, https://demo.proteusthemes.com/shaka/wp-content/uploads/sites/38/2016/06/1-360x204.jpg 360w" sizes="(max-width: 864px) 100vw, 864px">
+                                        </a>
+                                    </h5>
+                                    <h5>BASIC AND ADVANCED&nbsp;SURFING</h5>
+                                    <p>Basic and additional courses are for self taught surfers and amateur&nbsp;surfers who need more surfing knowledge or want to upgrade their skills.</p>
+                                    <p style="text-align: left">
+                                        <span style="color: #888888">
+                                            <strong>
+                                                <span class="icon-container">
+                                                    <span class="fa fa-check" style="color:#d80019;">
+                                                    </span>
+                                                </span> &nbsp;&nbsp;Getting to know the equipment
+                                            </strong>
+                                        </span>
+                                    </p>
+
+                                    <p style="text-align: left">
+                                        <span style="color: #888888">
+                                            <strong>
+                                                <span class="icon-container">
+                                                    <span class="fa fa-check" style="color:#d80019;">
+                                                    </span>
+                                                </span> &nbsp;&nbsp;Weather conditions and wind
+                                            </strong>
+                                        </span>
+                                    </p>
+                                    <p style="text-align: left">
+                                        <span style="color: #888888">
+                                            <strong>
+                                                <span class="icon-container">
+                                                    <span class="fa fa-check" style="color:#d80019;">
+                                                    </span>
+                                                </span> &nbsp;&nbsp;Motion techniques
+                                            </strong>
+                                        </span>
+                                    </p>
+                                    <p style="text-align: left">
+                                        <span style="color: #888888">
+                                            <strong>
+                                                <span class="icon-container">
+                                                    <span class="fa fa-check" style="color:#d80019;">
+                                                    </span>
+                                                </span> &nbsp;&nbsp;5h/day practice on water
+                                            </strong>
+                                        </span>
+                                    </p>
+                                    <a class="btn  btn-primary  col-xs-12" href="https://demo.proteusthemes.com/shaka/services/learn-how-to-surf/" target="_self"> BOOK LESSONS</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="col-md-6">
+
+
+            </div>
+        </div>
+
+    </div>
+</div>
