a07bcb2ed3f0223e3413cc72be83d2eeeb2af7b9
Trần Văn <tranvan1202@users.noreply.github.com>
2020-12-14 11:27:26 +0700

    fix 11

--- Danh sach modules/11/11-content.php
+++ Danh sach modules/11/11-content.php
@@ -15,84 +15,82 @@ $url_path = str_replace('\\', '/', $url_path);
     <div class="container">
         <div class="row">
 
-            <div id="pg-5-10" class="panel-grid panel-no-style">
-                <div id="pgc-5-10-0" class="panel-grid-cell">
-                    <div id="panel-5-10-0-0" class="so-panel widget widget_pw_testimonials widget-testimonials panel-first-child panel-last-child" data-index="19">
-                        <div class="testimonial__container">
-                            <h3 class="widget-title"><span class="widget-title__inline">Testimonials</span></h3>
-                            <div id="carousel-testimonials-widget-10-0-0" class="carousel  slide  testimonial" data-ride="carousel" data-interval="false">
-                                <div class="carousel-inner" role="listbox">
-                                    <div class="carousel-item active">
-                                        <div class="row">
-                                            <div class="col-xs-12  col-sm-12">
-                                                <blockquote>
-                                                    <p class="testimonial__quote"> I can't believe how much I had fun on this trip. I learned new techniques that I didn't even think were possible. I recommend anybody and everybody to come here and hit the waves with these guys. They are unbelievably friendly too!</p>
-                                                </blockquote>
-                                                <div class="testimonial__author-container"> <a class="testimonial__carousel  testimonial__carousel--left" href="#carousel-testimonials-widget-10-0-0" data-slide="prev"><i class="fa  fa-long-arrow-left" aria-hidden="true"></i><span class="sr-only" role="button">Next</span></a>
-                                                    <div class="testimonial__author"> <cite class="testimonial__author-name"> Grace Harris, </cite>
-                                                        <div class="testimonial__author-description"> Sydney surf girl</div>
-                                                    </div> <a class="testimonial__carousel  testimonial__carousel--right" href="#carousel-testimonials-widget-10-0-0" data-slide="next"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i><span class="sr-only" role="button">Previous</span></a>
-                                                    <div class="testimonial__rating"> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i></div>
-                                                </div>
-                                            </div>
+            <div class="widget-testimonials">
+                <div class="testimonial__container">
+                    <h3 class="widget-title"><span class="widget-title__inline">Testimonials</span></h3>
+                    <div id="carousel-testimonials-widget-10-0-0" class="carousel  slide  testimonial" data-ride="carousel" data-interval="false">
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                                <div class="row">
+                                    <div class="col-xs-12  col-sm-12">
+                                        <blockquote>
+                                            <p class="testimonial__quote"> I can't believe how much I had fun on this trip. I learned new techniques that I didn't even think were possible. I recommend anybody and everybody to come here and hit the waves with these guys. They are unbelievably friendly too!</p>
+                                        </blockquote>
+                                        <div class="testimonial__author-container"> <a class="testimonial__carousel  testimonial__carousel--left" href="#carousel-testimonials-widget-10-0-0" data-slide="prev"><i class="fa  fa-long-arrow-left" aria-hidden="true"></i><span class="sr-only" role="button">Next</span></a>
+                                            <div class="testimonial__author"> <cite class="testimonial__author-name"> Grace Harris, </cite>
+                                                <div class="testimonial__author-description"> Sydney surf girl</div>
+                                            </div> <a class="testimonial__carousel  testimonial__carousel--right" href="#carousel-testimonials-widget-10-0-0" data-slide="next"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i><span class="sr-only" role="button">Previous</span></a>
+                                            <div class="testimonial__rating"> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i></div>
                                         </div>
                                     </div>
-
-                                    <div class="carousel-item">
-                                        <div class="row">
-                                            <div class="col-xs-12  col-sm-12">
-                                                <blockquote>
-                                                    <p class="testimonial__quote"> I wasn't a very confident surfer before I came to stay here. Now that I have spent time learning with these guys, I feel ready to take on any challenge. They were so patient and knowledgable. I could not have had a better time anywhere in the world.</p>
-                                                </blockquote>
-                                                <div class="testimonial__author-container"> <a class="testimonial__carousel  testimonial__carousel--left" href="#carousel-testimonials-widget-10-0-0" data-slide="prev"><i class="fa  fa-long-arrow-left" aria-hidden="true"></i><span class="sr-only" role="button">Next</span></a>
-                                                    <div class="testimonial__author"> <cite class="testimonial__author-name"> Joshua Lee, </cite>
-                                                        <div class="testimonial__author-description"> Occasional surfer</div>
-                                                    </div> <a class="testimonial__carousel  testimonial__carousel--right" href="#carousel-testimonials-widget-10-0-0" data-slide="next"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i><span class="sr-only" role="button">Previous</span></a>
-                                                    <div class="testimonial__rating"> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="carousel-item">
-                                        <div class="row">
-                                            <div class="col-xs-12  col-sm-12">
-                                                <blockquote>
-                                                    <p class="testimonial__quote"> Amazing fun water experience, friendly and knowledgable staff. Lovely accomodation and it was easy to find the main building. Nightlife on the west coast near the see is amazing and wild. Will come again next year with friends.</p>
-                                                </blockquote>
-                                                <div class="testimonial__author-container"> <a class="testimonial__carousel  testimonial__carousel--left" href="#carousel-testimonials-widget-10-0-0" data-slide="prev"><i class="fa  fa-long-arrow-left" aria-hidden="true"></i><span class="sr-only" role="button">Next</span></a>
-                                                    <div class="testimonial__author"> <cite class="testimonial__author-name"> Jacob Robinson, </cite>
-                                                        <div class="testimonial__author-description"> Tour guide</div>
-                                                    </div> <a class="testimonial__carousel  testimonial__carousel--right" href="#carousel-testimonials-widget-10-0-0" data-slide="next"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i><span class="sr-only" role="button">Previous</span></a>
-                                                    <div class="testimonial__rating"> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="carousel-item">
-                                        <div class="row">
-                                            <div class="col-xs-12  col-sm-12">
-                                                <blockquote>
-                                                    <p class="testimonial__quote"> Came as customers, left as friend. It was a blast to be in resort for one week. Perfect weather, perfect waves, lots of activities. I need to come again to improve my diving and climbing skills.</p>
-                                                </blockquote>
-                                                <div class="testimonial__author-container"> <a class="testimonial__carousel  testimonial__carousel--left" href="#carousel-testimonials-widget-10-0-0" data-slide="prev"><i class="fa  fa-long-arrow-left" aria-hidden="true"></i><span class="sr-only" role="button">Next</span></a>
-                                                    <div class="testimonial__author"> <cite class="testimonial__author-name"> Ella Thomas, </cite>
-                                                        <div class="testimonial__author-description"> Local</div>
-                                                    </div> <a class="testimonial__carousel  testimonial__carousel--right" href="#carousel-testimonials-widget-10-0-0" data-slide="next"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i><span class="sr-only" role="button">Previous</span></a>
-                                                    <div class="testimonial__rating"> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    
                                 </div>
                             </div>
+
+                            <div class="carousel-item">
+                                <div class="row">
+                                    <div class="col-xs-12  col-sm-12">
+                                        <blockquote>
+                                            <p class="testimonial__quote"> I wasn't a very confident surfer before I came to stay here. Now that I have spent time learning with these guys, I feel ready to take on any challenge. They were so patient and knowledgable. I could not have had a better time anywhere in the world.</p>
+                                        </blockquote>
+                                        <div class="testimonial__author-container"> <a class="testimonial__carousel  testimonial__carousel--left" href="#carousel-testimonials-widget-10-0-0" data-slide="prev"><i class="fa  fa-long-arrow-left" aria-hidden="true"></i><span class="sr-only" role="button">Next</span></a>
+                                            <div class="testimonial__author"> <cite class="testimonial__author-name"> Joshua Lee, </cite>
+                                                <div class="testimonial__author-description"> Occasional surfer</div>
+                                            </div> <a class="testimonial__carousel  testimonial__carousel--right" href="#carousel-testimonials-widget-10-0-0" data-slide="next"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i><span class="sr-only" role="button">Previous</span></a>
+                                            <div class="testimonial__rating"> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item">
+                                <div class="row">
+                                    <div class="col-xs-12  col-sm-12">
+                                        <blockquote>
+                                            <p class="testimonial__quote"> Amazing fun water experience, friendly and knowledgable staff. Lovely accomodation and it was easy to find the main building. Nightlife on the west coast near the see is amazing and wild. Will come again next year with friends.</p>
+                                        </blockquote>
+                                        <div class="testimonial__author-container"> <a class="testimonial__carousel  testimonial__carousel--left" href="#carousel-testimonials-widget-10-0-0" data-slide="prev"><i class="fa  fa-long-arrow-left" aria-hidden="true"></i><span class="sr-only" role="button">Next</span></a>
+                                            <div class="testimonial__author"> <cite class="testimonial__author-name"> Jacob Robinson, </cite>
+                                                <div class="testimonial__author-description"> Tour guide</div>
+                                            </div> <a class="testimonial__carousel  testimonial__carousel--right" href="#carousel-testimonials-widget-10-0-0" data-slide="next"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i><span class="sr-only" role="button">Previous</span></a>
+                                            <div class="testimonial__rating"> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="carousel-item">
+                                <div class="row">
+                                    <div class="col-xs-12  col-sm-12">
+                                        <blockquote>
+                                            <p class="testimonial__quote"> Came as customers, left as friend. It was a blast to be in resort for one week. Perfect weather, perfect waves, lots of activities. I need to come again to improve my diving and climbing skills..................</p>
+                                        </blockquote>
+                                        <div class="testimonial__author-container"> <a class="testimonial__carousel  testimonial__carousel--left" href="#carousel-testimonials-widget-10-0-0" data-slide="prev"><i class="fa  fa-long-arrow-left" aria-hidden="true"></i><span class="sr-only" role="button">Next</span></a>
+                                            <div class="testimonial__author"> <cite class="testimonial__author-name"> Ella Thomas, </cite>
+                                                <div class="testimonial__author-description"> Local</div>
+                                            </div> <a class="testimonial__carousel  testimonial__carousel--right" href="#carousel-testimonials-widget-10-0-0" data-slide="next"><i class="fa  fa-long-arrow-right" aria-hidden="true"></i><span class="sr-only" role="button">Previous</span></a>
+                                            <div class="testimonial__rating"> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i> <i class="fa  fa-star"></i></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
                 </div>
             </div>
+
+
         </div>
     </div>
 </div>
