bc12298ee44db02e2e2d68cb1612337a6d0a773b
tranvan1202 <bshine619@gmail.com>
2020-12-19 04:46:52 +0700

    fix 44

--- Danh sach modules/44/44-content.php
+++ Danh sach modules/44/44-content.php
@@ -13,82 +13,109 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-44">
     <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
-                    <figure class="gallery-item">
-                        <div class="gallery-icon landscape">
-                            <a href="#" data-featherlight="image">
-                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
-                            </a>
-                        </div>
-                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
-                    </figure>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
-                    <figure class="gallery-item">
-                        <div class="gallery-icon landscape">
-                            <a href="#" data-featherlight="image">
-                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
-                            </a>
-                        </div>
-                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
-                    </figure>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
-                    <figure class="gallery-item">
-                        <div class="gallery-icon landscape">
-                            <a href="#" data-featherlight="image">
-                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
-                            </a>
-                        </div>
-                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
-                    </figure>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
-                    <figure class="gallery-item">
-                        <div class="gallery-icon landscape">
-                            <a href="#" data-featherlight="image">
-                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
-                            </a>
-                        </div>
-                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
-                    </figure>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
-                    <figure class="gallery-item">
-                        <div class="gallery-icon landscape">
-                            <a href="#" data-featherlight="image">
-                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
-                            </a>
-                        </div>
-                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
-                    </figure>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
-                    <figure class="gallery-item">
-                        <div class="gallery-icon landscape">
-                            <a href="#" data-featherlight="image">
-                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
-                            </a>
-                        </div>
-                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
-                    </figure>
-                </div>
-            </div>
-        
+        <div id="gallery-lightbox" class="row portfolio__gallery" data-toggle="modal" data-target="#exampleModal">
 
+            <figure class="portfolio__gallery-item  gallery-item">
+                <img class="img-slide img-fluid" src="./images/44-images1.jpg" alt="First slide" data-target="#carouselExample" width="540" height="304" data-slide-to="0">
+
+                <figcaption class="portfolio__gallery-item-caption  wp-caption-text gallery-caption"> Sandy beach resort</figcaption>
+            </figure>
+
+
+
+            <figure class="portfolio__gallery-item  gallery-item">
+                <img class="img-slide img-fluid" src="./images/44-images2.jpg" alt="Second slide" data-target="#carouselExample" width="540" height="304" data-slide-to="1">
+
+            </figure>
+
+
+
+            <figure class="portfolio__gallery-item  gallery-item">
+                <img class="img-slide img-fluid" src="./images/44-images3.jpg" alt="Third slide" data-target="#carouselExample" width="540" height="304" data-slide-to="2">
+                <figcaption class="portfolio__gallery-item-caption  wp-caption-text gallery-caption"> Sandy beach resort</figcaption>
+            </figure>
+
+
+
+            <figure class="portfolio__gallery-item  gallery-item">
+                <img class="img-slide img-fluid" src="./images/44-images4.jpg" alt="Fourth slide" data-target="#carouselExample" width="540" height="304" data-slide-to="3">
+            </figure>
+
+
+
+            <figure class="portfolio__gallery-item  gallery-item">
+                <img class="img-slide img-fluid" src="./images/44-images5.jpg" alt="Fifth slide" data-target="#carouselExample" width="540" height="304" data-slide-to="4">
+                <figcaption class="portfolio__gallery-item-caption  wp-caption-text gallery-caption"> Sandy beach resort</figcaption>
+            </figure>
+
+
+
+            <figure class="portfolio__gallery-item  gallery-item">
+                <img class="img-slide img-fluid" src="./images/44-images6.jpg" alt="Sixth slide" data-target="#carouselExample" width="540" height="304" data-slide-to="5">
+            </figure>
 
         </div>
+
+
+        <div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
+            <button type="button" class="close m-0 p-3 text-white position-absolute right-0" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content bg-transparent">
+                    <div class="modal-body p-0">
+                        <div id="carouselExample" class="carousel slide carousel-fade" data-ride="false">
+                            <div class="carousel-item active">
+                                <!-- <img class="d-block w-100" src="./images/44-images1.jpg" alt="First slide"> -->
+                                <figure class="modal-gallery-item">
+                                    <img class="d-block w-100" src="./images/44-images1.jpg" alt="First slide">
+                                    <figcaption class="modal-gallery-caption"> Sandy beach resort</figcaption>
+                                </figure>
+                            </div>
+                            <div class="carousel-item">
+                                <figure class="modal-gallery-item">
+                                    <img class="d-block w-100" src="./images/44-images2.jpg" alt="Second slide">
+                                </figure>
+
+                            </div>
+                            <div class="carousel-item">
+                                <figure class="modal-gallery-item">
+                                    <img class="d-block w-100" src="./images/44-images3.jpg" alt="Third slide">
+                                    <figcaption class="modal-gallery-caption"> Sandy beach resort</figcaption>
+                                </figure>
+                            </div>
+
+                            <div class="carousel-item">
+                                <figure class="modal-gallery-item">
+                                    <img class="d-block w-100" src="./images/44-images4.jpg" alt="Fourth slide">
+                                </figure>
+                            </div>
+                            <div class="carousel-item">
+                                <figure class="modal-gallery-item">
+                                    <img class="d-block w-100" src="./images/44-images5.jpg" alt="Fifth slide">
+                                    <figcaption class="modal-gallery-caption"> Sandy beach resort</figcaption>
+                                </figure>
+                            </div>
+
+                            <div class="carousel-item">
+                                <figure class="modal-gallery-item">
+                                    <img class="d-block w-100" src="./images/44-images6.jpg" alt="Sixth slide">
+                                </figure>
+                            </div>
+                        </div>
+
+                        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
