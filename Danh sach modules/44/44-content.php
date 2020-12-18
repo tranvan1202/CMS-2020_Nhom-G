7369f72fc3419d083b3e39ee8fbf718521b9b0de
Trần Văn <tranvan1202@users.noreply.github.com>
2020-12-18 16:26:45 +0700

    44

---
+++ Danh sach modules/44/44-content.php
@@ -0,0 +1,94 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-44">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
+                    <figure class="gallery-item">
+                        <div class="gallery-icon landscape">
+                            <a href="#" data-featherlight="image">
+                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
+                            </a>
+                        </div>
+                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
+                    <figure class="gallery-item">
+                        <div class="gallery-icon landscape">
+                            <a href="#" data-featherlight="image">
+                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
+                            </a>
+                        </div>
+                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
+                    <figure class="gallery-item">
+                        <div class="gallery-icon landscape">
+                            <a href="#" data-featherlight="image">
+                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
+                            </a>
+                        </div>
+                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
+                    <figure class="gallery-item">
+                        <div class="gallery-icon landscape">
+                            <a href="#" data-featherlight="image">
+                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
+                            </a>
+                        </div>
+                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
+                    <figure class="gallery-item">
+                        <div class="gallery-icon landscape">
+                            <a href="#" data-featherlight="image">
+                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
+                            </a>
+                        </div>
+                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
+                    </figure>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div id="gallery-1" class="gallery galleryid-271 gallery-columns-2 gallery-size-medium">
+                    <figure class="gallery-item">
+                        <div class="gallery-icon landscape">
+                            <a href="#" data-featherlight="image">
+                                <img width="300" height="169" src="./images/44-images.jpg" class="attachment-medium size-medium" alt="" loading="lazy" aria-describedby="gallery-1-97" srcset="./images/44-images.jpg 300w, ./images/44-images.jpg 768w, ./images/44-images.jpg 540w, ./images/44-images.jpg 352w, ./images/44-images.jpg 360w, ./images/44-images.jpg 864w" sizes="(max-width: 300px) 100vw, 300px">
+                            </a>
+                        </div>
+                        <figcaption class="wp-caption-text gallery-caption" id="gallery-1-97"> Banana water ride</figcaption>
+                    </figure>
+                </div>
+            </div>
+        
+
+
+        </div>
+    </div>
+</div>
