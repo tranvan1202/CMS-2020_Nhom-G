4da44f242797193a316c4a2c8b26f64ac6187331
Trần Văn <tranvan1202@users.noreply.github.com>
2020-12-26 01:30:51 +0700

    test ghép module

---
+++ cms-theme-test-module/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package cms-theme-test-module
+ */
+
+?>
+
+<?php get_template_part('includes/15/15', 'content');?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
