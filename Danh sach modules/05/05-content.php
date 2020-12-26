7d91c7e12135a683955270782628ddb315cfafcd
Thắng Lê <74522109+thangle96@users.noreply.github.com>
2020-12-26 13:25:12 +0700

    update module05

--- Danh sach modules/05/05-content.php
+++ Danh sach modules/05/05-content.php
@@ -11,17 +11,137 @@ $url_path = $url_host . $matches[1][0];
 
 $url_path = str_replace('\\', '/', $url_path);
 ?>
-<li class="clearfix hentry product type-product post-1143 status-publish first instock product_cat-beach-accesories product_tag-ball has-post-thumbnail shipping-taxable purchasable product-type-simple">
-    <div class="row">
-        <div class="col-6 col-md-4 col-lg-3 mb-3">
-            <div class="item">
-                <a href="#">
-                    <img class="hinh" width="300" height="300" src="./images/shop5.jpg">
-                    <h2>Beach Air Ball</h2>
-                    <span class="price">£ 9.99</span>
-                </a>
-                <a href="" class="form-control btn btn-info add-card">ADD TO CARD</a>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/05.css">
+    <link rel="stylesheet" href="./public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./public/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="content-active">
+            <h3>Our Trips</h3>
+            <div class="link-active">
+                <a href="#" class="active">ALL</a>
+                <a href="#">SURFING</a>
+                <a href="#">FAMILY TRIPS</a>
+                <a href="#">WINSURFING</a>
+                <a href="#">KITESURFING</a>
             </div>
         </div>
+        <div class="slidesgr">
+            <div class="slides">
+                <div class="slide">
+                    <div class="content">
+                        <img src="./public/images/project8-352x198.jpg" alt="img1" class="img-fluid">
+                        <div class="allTextContent">
+                            <strong class="allContent">
+                                <i class="dotted">SURFING VACATION</i>
+                            </strong> <br>
+                            <p></p>
+                            <i class="fa fa-map-marker" aria-hidden="true"> Bải biển Đen, San Diego</i>
+                            <i class="fa fa-info" aria-hidden="true"> Thiết bị được cung cấp</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="content">
+                        <img src="./public/images/project9-352x198.jpg" alt="img1" class="img-fluid">
+                        <div class="allTextContent">
+                            <strong class="allContent"><i class="dotted">FAMILI WATER SPORTS</i></strong> <br>
+                            <p></p>
+                            <i class="fa fa-map-marker" aria-hidden="true"> Bải biển Đen, San Diego</i>
+                            <i class="fa fa-info" aria-hidden="true"> Thiết bị được cung cấp</i>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="slide">
+                    <div class="content">
+                        <img src="./public/images/project10-352x198.jpg" alt="img1" class="img-fluid">
+                        <div class="allTextContent">
+                            <strong class="allContent"><i class="dotted">YOUNG GUNS COURSES</i></strong> <br>
+                            <p></p>
+                            <i class="fa fa-map-marker" aria-hidden="true"> Bải biển Đen, San Diego</i>
+                            <i class="fa fa-info" aria-hidden="true"> Thiết bị được cung cấp</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="content">
+                        <img src="./public/images/project7-352x198.jpg" alt="img1" class="img-fluid">
+                        <div class="allTextContent">
+                            <strong class="allContent"><i class="dotted">WINSURFING FREESTYLE</i></strong> <br>
+                            <p></p>
+                            <i class="fa fa-map-marker" aria-hidden="true"> Bải biển Đen, San Diego</i>
+                            <i class="fa fa-info" aria-hidden="true"> Thiết bị được cung cấp</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="content">
+                        <img src="./public/images/project5-352x198.jpg" alt="img1" class="img-fluid">
+                        <div class="allTextContent">
+                            <strong class="allContent"><i class="dotted">SURFING FOR BEGINNERS</i></strong> <br>
+                            <p></p>
+                            <i class="fa fa-map-marker" aria-hidden="true"> Bải biển Đen, San Diego</i>
+                            <i class="fa fa-info" aria-hidden="true"> Thiết bị được cung cấp</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="content">
+                        <img src="./public/images/project3-352x198.jpg" alt="img1" class="img-fluid">
+                        <div class="allTextContent">
+                            <strong class="allContent"><i class="dotted">ADVANCED KITESURFING</i></strong> <br>
+                            <p></p>
+                            <i class="fa fa-map-marker" aria-hidden="true"> Bải biển Đen, San Diego</i>
+                            <i class="fa fa-info" aria-hidden="true"> Thiết bị được cung cấp</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="content">
+                        <img src="./public/images/project6-352x198.jpg" alt="img1" class="img-fluid">
+                        <div class="allTextContent">
+                            <strong class="allContent"><i class="dotted">PAROS ISLAND</i></strong> <br>
+                            <p></p>
+                            <i class="fa fa-map-marker" aria-hidden="true"> Bải biển Đen, San Diego</i>
+                            <i class="fa fa-info" aria-hidden="true"> Thiết bị được cung cấp</i>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="content">
+                        <img src="./public/images/project2-352x198.jpg" alt="img1" class="img-fluid">
+                        <div class="allTextContent">
+                            <strong class="allContent"><i class="dotted">TRIP TO SPAN</i></strong> <br>
+                            <p></p>
+                            <i class="fa fa-map-marker" aria-hidden="true"> Bải biển Đen, San Diego</i>
+                            <i class="fa fa-info" aria-hidden="true"> Thiết bị được cung cấp</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <a class="prev" onclick="changeslide(-1)"><i class="fa fa-arrow-left" aria-hidden="true"></i>
+        </a>
+        <a class="next" onclick="changeslide(1)"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
     </div>
-</li>
+    <br>
+    <script src="./public/js/bootstrap.min.js"></script>
+    <script src="./public/js/bootstrap.js"></script>
+    <script src="./public/js/jquery-2.1.4.min.js"></script>
+    <script src="./index.js"></script>
+</body>
+
+</html>
